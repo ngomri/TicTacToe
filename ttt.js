513b538b13b43cf555e547b6746c1bbd8b3f7b83
ngomri <57151912+ngomri@users.noreply.github.com>
2020-05-04 17:43:46 +0100

    Add files via upload

--- ttt.js
+++ ttt.js
@@ -1,27 +1,94 @@
 
 //set variables
 
+var tds = document.querySelectorAll("td");
+
+var X = document.getElementById("X");
+var O = document.getElementById("O");
+
 var human;
 var computer;
 
-var firstPlayer = [1,'X']; // [player, sign]
-var secondPlayer = [2,'O'];
+var firstPlayer = 'X';
+var secondPlayer = 'O';
+       
+X.style.color = "black";
+human = firstPlayer;
+computer = secondPlayer;
 
-var current;
-var firstTurn;
+X.addEventListener("click", function() {
+    X.style.color = "black";
+    O.style.color = "#40468C";
+    human = firstPlayer;
+    computer = secondPlayer;
+})
 
-var winner = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+O.addEventListener("click", function() {
+    O.style.color = "black";
+    X.style.color = "#40468C";
+    human = secondPlayer;
+    computer = firstPlayer;
 
-var board = ['#','#','#','#','#','#','#','#','#']
-var gameWon = false;
-var turnCount = 0;
+
+})
+
+ function computerTurn() {
+    var computerChoice = Math.floor(Math.random() * 8);
+     while(tds[computerChoice].innerHTML !== "") {
+        computerChoice = Math.floor(Math.random() * 8);
+     }
+     tds[computerChoice].innerHTML = computer;
+     if (isWinner(computer) === true) {
+         displayWinner(computer);
+     }
+
+ }
+
+function cellClicked(cell)
+{
+    console.log(cell)
+    cell.innerHTML = human;
+    if (isWinner(human) === true) {
+        displayWinner(human);
+        return
+    }
+    computerTurn()
+
+}
+
+function displayWinner(player) {
+    console.log("You won", player)
+}
+
+
+function isWinner(player) {
+    var winning = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+    for(var i = 0; i < winning.length; i++) {
+        x = winning[i]
+        allMatch = true;
+        for(var n = 0; n < x.length; n++) {
+            // console.log(i,n,tds[x[n]])
+            if(tds[x[n]].innerHTML !== player){
+                allMatch = false;
+            }
+        }
+        if(allMatch)
+            return true;
+    }
+    return false;
+}
+
+for(var i = 0; i < tds.length; i++)
+{
+   tds[i].addEventListener("click", function() {
+    cellClicked(this)
+
+});
+}
+// td.addEventListener("click", function() {
+//     cellClicked(this)
+
+// });
+
+
 
